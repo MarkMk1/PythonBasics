f5cdb1649677a8bb9175388b158b121d61d22f33
MarkMk1 <markbradyeire@gmail.com>
2016-08-08 20:59:59 +0100

    Finished Wordcounter
    Minus webcrawling

--- WordCount.py
+++ WordCount.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import operator
 
 
 def start(url):
@@ -11,20 +12,32 @@ def start(url):
         content = post_text.string
         words = content.lower().split()
         for each_word in words:
-            print(each_word)
             word_list.append(each_word)
     word_cleanup(word_list)
 
 
-def word_cleanup(trash):
+def word_cleanup(unsorted):
     clean_word_list = []
-    for clean_word in trash:
+    for clean_word in unsorted:
         symbols = "!£$%^*&()_+=-`¬|,.<>/?;:'@.#~[{]}\""
         for i in range(0, len(symbols)):
             clean_word = clean_word.replace(symbols[i], "")
         if len(clean_word) > 0:
-            print(clean_word)
-            clean_word_list.append(clean_word_list)
+            clean_word_list.append(clean_word)
+    dictionary_count(clean_word_list)
 
+# used clean_word_list, got [...]
+# means product repeats within self
+
+
+def dictionary_count(clean_list):
+    dictionary = {}
+    for clean_words in clean_list:
+        if clean_words in dictionary:
+            dictionary[clean_words] += 1
+        else:
+            dictionary[clean_words] = 1
+    for key, value in sorted(dictionary.items(), key=operator.itemgetter(1)):
+        print(key, value)
 
 start("https://cgcookie.com/courses")
